eaaa41f036b91c982c0780e50680d99f296701b1
Allison Lee <allison.lee124@gmail.com>
2020-06-18 13:21:12 -0700

    Change logo and handle form submit

--- src/Search.js
+++ src/Search.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 
-class Search extends Component {
-  render() {
+const Search = (props) => {
+    const [input, setInput] = useState("");
+
+    const handleSubmit = (evt) => {
+      evt.preventDefault();
+      console.log(input);
+  }
+
     return (
       <div className='dropdown'>
         <section className='search-content'>
           <h2 className='search-title'>Search by keyword</h2>
           <h4 className='search-subtext'>Enter ingredients that you have at home! Example: chicken spinach butter</h4>
           <div>
-            <form className='search-wrapper'>
-              <input type="text" placeholder="Search..." className="search"/>
+            <form className='search-wrapper' onSubmit={handleSubmit}>
+              <input type="text" placeholder="Search..." className="search" value={input} onChange={e => setInput(e.target.value)}/>
               <button type="submit" className="submit">Submit</button>
             </form>
           </div>
         </section>
       </div>
     );
-  }
 }
 
 export default Search;
